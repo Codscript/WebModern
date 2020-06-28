d988287c0bc8f253caaa90cdb3ce945526354661
Codscript <mussa.monteiro@gmail.com>
2020-06-28 18:36:57 -0300

    Numbers alguns cuidados

--- numerosAlgunsCudados.js
+++ numerosAlgunsCudados.js
@@ -2,10 +2,16 @@ console.log(7 / 2);
 // infinity
 console.log("10" / 2);
 // 5
+console.log('3' - 2);
+// 1
+console.log('3' + 2);// Valida string primeiro
+// 32
 console.log("Show!" / 2);
 // NaN
 console.log(0.1 + 0.7);
 // 0.7999999999999999
+
 // console.log(10.toStrng); 
+
 console.log((10.345).toFixed(2));
 // 10.35
