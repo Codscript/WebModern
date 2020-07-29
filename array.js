3cd956a6d537e8dd948bb1268f7f896bb09e89f6
Codscript <mussa.monteiro@gmail.com>
2020-07-29 08:59:05 -0300

    Array

--- array.js
+++ array.js
@@ -8,3 +8,9 @@ console.log(valores.length);
 
 valores.push({ id: 3 }, false, null, 'teste')
 console.log(valores);
+
+console.log(valores.pop());
+delete valores[0]
+console.log(valores);
+
+console.log(typeof valores);
